0d20f471625dc3945ca5a42d27728ce090e213c5
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2024-02-17 22:55:20 +0200

    navbar done

--- src/app/features/dashboard/components/navbar/navbar.component.ts
+++ src/app/features/dashboard/components/navbar/navbar.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  faBell = faBell;
+  faMagnifyingGlass = faMagnifyingGlass;
+  searchKeyword: string = '';
+}
--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -6,9 +6,20 @@ import { LayoutComponent } from './layout/layout.component';
 import { TasksModule } from './features/tasks/tasks.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [LayoutComponent, SidebarComponent, NavbarComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), TasksModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    TasksModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule,
+    InputTextModule,
+  ],
 })
 export class DashboardModule {}
