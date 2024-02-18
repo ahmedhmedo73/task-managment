ace62cbe2222677530ee40535d2ae2b6b400dbb5
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2024-02-18 07:34:47 +0200

    layout done

--- src/app/features/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/features/dashboard/components/sidebar/sidebar.component.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  links: any[] = [
+    {
+      label: 'dashboard',
+      icon: 'assets/images/sidebar/dashboard.svg',
+      routerLink: '/dashboard',
+    },
+    {
+      label: 'projects',
+      icon: 'assets/images/sidebar/projects.svg',
+      routerLink: '/projects',
+    },
+    {
+      label: 'tasks',
+      icon: 'assets/images/sidebar/tasks.svg',
+      routerLink: '/tasks',
+    },
+    {
+      label: 'calendar',
+      icon: 'assets/images/sidebar/calendar.svg',
+      routerLink: '/calendar',
+    },
+  ];
+}
