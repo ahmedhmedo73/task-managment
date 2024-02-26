ed3a711bcd8849daa89ceda7a1453b955548b2d9
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2024-02-26 10:15:15 +0200

    ..

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,10 +3,10 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   let _token =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im50dXNlciIsInJvbGUiOiJhZG1pbiIsIm5iZiI6MTcwNTExMjcwMiwiZXhwIjoxNzA1MTE1NzAyLCJpYXQiOjE3MDUxMTI3MDJ9.Z375fERRhzw9aEyzQ8uKw0N76IZ0gSQHv0sHfZ86Wjs';
-  let jwtToken = req.clone({
+  let request = req.clone({
     setHeaders: {
       Authorization: 'bearer ' + _token,
     },
   });
-  return next(jwtToken);
+  return next(request);
 };
