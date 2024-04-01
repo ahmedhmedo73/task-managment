c9b717773301ce7d286578cd07032ca52e45cdda
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2024-04-01 13:37:34 +0200

    og for products page

--- src/app/features/dashboard/features/products/products.component.ts
+++ src/app/features/dashboard/features/products/products.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
-
+  constructor(private meta: Meta, private title: Title) {
+    title.setTitle('My Awesome Angular 17 SSR App');
+    meta.addTags([
+      { name: 'og:title', content: 'My Awesome Angular 17 SSR App' },
+      { name: 'og:description', content: 'A detailed description of my Angular 17 SSR app content.' },
+      { name: 'og:image', content: 'https://example.com/my-image.jpg' },
+    ]);
+  }
 }
