8bc43c6e0d2d720b908e51881555010733c4301b
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2024-04-01 16:10:52 +0200

    change og meta

--- src/app/features/dashboard/features/products/products.component.ts
+++ src/app/features/dashboard/features/products/products.component.ts
@@ -7,12 +7,20 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
-  constructor(private meta: Meta, private title: Title) {
-    title.setTitle('My Awesome Angular 17 SSR App');
-    meta.addTags([
-      { name: 'og:title', content: 'My Awesome Angular 17 SSR App' },
-      { name: 'og:description', content: 'A detailed description of my Angular 17 SSR app content.' },
-      { name: 'og:image', content: 'https://example.com/my-image.jpg' },
-    ]);
+  constructor(private meta: Meta, private title: Title) {}
+  ngOnInit(): void {
+    this.title.setTitle('My Awesome Angular 17 SSR App');
+    this.meta.updateTag({
+      property: 'og:title',
+      content: 'My Awesome Angular 17 SSR App',
+    });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: 'A detailed description of my Angular 17 SSR app content.',
+    });
+    this.meta.updateTag({
+      property: 'og:image',
+      content: 'https://example.com/my-image.jpg',
+    });
   }
 }
